50154d4facb32ba41336c28f937daa343ce06232
UddipanBiswas <118747206+UddipanBiswas@users.noreply.github.com>
2024-08-22 13:54:28 +0530

    Create operators.kt
    understanding operators in kotlin

---
+++ operators.kt
@@ -0,0 +1,41 @@
+fun main()
+{
+    var a =10
+    var b= 5 
+    // Arithmetic operators
+    val add = a + b // or a.plus(b)
+    println(add)
+    val substract = a - b // or a.minus(b)
+    println(substract)
+    val div = a/b // or a.div(b)
+    println(div)
+    val multiply = a*b // or a.times(b)
+    println(multiply)
+    val reminder = a%b // or a.rem(b)
+    println(reminder)
+    
+    //Relational operators
+    
+    val greater = a>b
+    println(greater)
+    val lessthan = a<b
+    println(lessthan)
+    val greater_equal = a>=b
+    println(greater_equal)
+    val lessthan_equal = a<=b
+    println(lessthan_equal)
+    val isequal = a==b
+    println(isequal)
+    val notequal = a!=b
+    println(notequal)
+    
+    // Unary operators
+    val plus = +a
+    println(plus)
+     val minus = -a
+    println(minus)
+     val increment = ++a
+    println(increment)
+     val decrement = --a
+    println(decrement)  
+}
