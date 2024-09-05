9901dd29017c92c6308b0db37e563908ffc5659e
UddipanBiswas <118747206+UddipanBiswas@users.noreply.github.com>
2024-09-05 13:39:14 +0530

    discount program.kt

---
+++ discount program.kt
@@ -0,0 +1,17 @@
+fun main() {
+
+    val total=1000
+    val member = true
+    if(total>=1000){
+        if(member){
+            println("you got a discount of 20%")
+        }
+        else{
+            println("you got a discount of 10%")
+        }  
+    }
+    else{
+        println("no discount")
+    }
+}
+    
